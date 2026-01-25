f78edfa478cb713c73c0d058a79546b014cb9d21
Suwitch <18065143863@163.com>
2026-01-25 14:26:05 +0800

    chore: initialize project with basic framework

---
+++ src/main/java/com/example/computerassociation/ComputerAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.example.computerassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerAssociationApplication.class, args);
+	}
+
+}
