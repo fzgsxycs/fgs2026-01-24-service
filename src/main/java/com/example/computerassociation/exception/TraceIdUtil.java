4e8b906bb695e2f27f578ec430b1b1de97a1dbb2
suiye <qulan3723@163.com>
2026-02-03 21:01:23 +0800

    异常的处理

--- src/main/java/com/example/computerassociation/exception/TraceIdUtil.java
+++ src/main/java/com/example/computerassociation/exception/TraceIdUtil.java
@@ -1,8 +1,5 @@
 package com.example.computerassociation.exception;
 
-
-import cn.hutool.core.util.IdUtil;
-
 public class TraceIdUtil {
 
     private static final ThreadLocal<String> TRACE_ID_THREAD_LOCAL=new ThreadLocal<>();
