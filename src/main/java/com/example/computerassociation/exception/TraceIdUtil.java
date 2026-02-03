b95748aa0b48ed6e0f9332929ccb1b2d36488aa3
suiye <qulan3723@163.com>
2026-02-03 21:01:05 +0800

    异常的处理

--- src/main/java/com/example/computerassociation/exception/TraceIdUtil.java
+++ src/main/java/com/example/computerassociation/exception/TraceIdUtil.java
@@ -7,14 +7,8 @@ public class TraceIdUtil {
 
     private static final ThreadLocal<String> TRACE_ID_THREAD_LOCAL=new ThreadLocal<>();
 
-    public static void generateAndSetTraceId(){
-        String traceId=IdUtil.fastSimpleUUID();
-        TRACE_ID_THREAD_LOCAL.set(traceId);
-    }
     public static String getTraceId(){
         return TRACE_ID_THREAD_LOCAL.get();
     }
-    public static void removeTraceId(){
-        TRACE_ID_THREAD_LOCAL.remove();
-    }
+
 }
